56ab29b7c2799df9416dc48ce1e44ba36d5ef54a
mac <mac@macs-MacBook-Pro.local>
2022-05-19 17:48:44 +0300

    kavalier app

--- kavalier/src/app/app.module.ts
+++ kavalier/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from '@angular/material/input'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CheckAvailabilityComponent } from './check-availability/check-availability.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CheckAvailabilityComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    NoopAnimationsModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatNativeDateModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ kavalier/src/app/check-availability/check-availability.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-check-availability',
+  templateUrl: './check-availability.component.html',
+  styleUrls: ['./check-availability.component.scss']
+})
+
+export class CheckAvailabilityComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  checkFunction() {
+    return console.log(`Arrive`)
+  }
+}
